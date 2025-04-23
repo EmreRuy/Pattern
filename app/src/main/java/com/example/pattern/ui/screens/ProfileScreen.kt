9fbd715f5f9300ed09adcd189a4c44e9cd7aba02
EmreRuy <uyar.em.eu@gmail.com>
2025-04-23 17:10:03 +0300

    Profile Screen additions

--- app/src/main/java/com/example/pattern/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/pattern/ui/screens/ProfileScreen.kt
@@ -1,7 +1,9 @@
 package com.example.pattern.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,17 +13,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,15 +37,17 @@ import com.example.pattern.R
 
 @Preview
 @Composable
-fun ProfileScreenPreview(){
+fun ProfileScreenPreview() {
     ProfileScreen()
 }
 
 @Composable
 fun ProfileScreen() {
+    val scroll = rememberScrollState()
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(scroll)
             .background(MaterialTheme.colorScheme.background)
             .padding(vertical = 8.dp)
     ) {
@@ -71,9 +80,34 @@ fun ProfileScreen() {
                 tint = MaterialTheme.colorScheme.onBackground
             )
         }
-
         Spacer(modifier = Modifier.height(16.dp))
-
+        Row(
+            modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Habits completed",
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.primary.copy(alpha = 0.1f),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .clip(RoundedCornerShape(12.dp))
+                    .padding(horizontal = 12.dp, vertical = 6.dp)
+            ) {
+                Text(
+                    text = "Weekly",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+        }
         Card(
             modifier = Modifier
                 .fillMaxWidth()
@@ -88,16 +122,16 @@ fun ProfileScreen() {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(24.dp)
+                    .padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
-                    text = "Welcome to your profile!",
+                    text = "Analytics",
                     style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
                     color = MaterialTheme.colorScheme.onSurface
                 )
-
                 Spacer(modifier = Modifier.height(8.dp))
-
                 Text(
                     text = "This is a large card where you can add more content like user info, stats, buttons, etc.",
                     style = MaterialTheme.typography.bodyMedium,
